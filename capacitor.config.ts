b924b7cf10750c80304ee992f20abeb7c8025ce3
Anthony Paredes <anthonyparedes1080@gmail.com>
2026-01-14 04:26:31 +0000

    Debug failed run build in Ionic Appflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.grupo8dawm2025pao2.com',
-  appName: '\x13ClimaApp',
+  appName: 'ClimaApp',
   webDir: 'dist'
 };
 
